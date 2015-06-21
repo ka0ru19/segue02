8b8c5fb861f10416e6591721308e625c075877fe
井上航 <k.prfm19@gmail.com>
2015-06-21 19:17:21 +0900

    新規写真の登録を実装中

--- SegueTableViewDetailPic02/DetailViewController.swift
+++ SegueTableViewDetailPic02/DetailViewController.swift
@@ -8,11 +8,11 @@
 
 import UIKit
 
-class DetailViewController: UIViewController{
+class DetailViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet var imageView: UIImageView?
     @IBOutlet weak var nameLabel: UILabel!
-    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var dataLabel: UILabel!
     
     var selectedImg: UIImage?
     var name: String?
@@ -25,14 +25,39 @@ class DetailViewController: UIViewController{
 //        self.navigationItem.backBarButtonItem = backButtonItem
         //コードで戻るボタンを生成　ここまで
         
-        imageView?.image = selectedImg
-        // 画像のアスペクト比を維持しUIImageViewサイズに収まるように表示
-        imageView?.contentMode = UIViewContentMode.ScaleAspectFit
-        
-        //新規作成の場合
+        if selectedImg == nil { //新規作成の場合
+            self.pickImageFromCamera()
+            self.pickImageFromLibrary()
+            
+        }else{
+            imageView?.image = selectedImg
+            // 画像のアスペクト比を維持しUIImageViewサイズに収まるように表示
+            imageView?.contentMode = UIViewContentMode.ScaleAspectFit
+        }
         
     }
     
+    // 写真を撮ってそれを選択
+    func pickImageFromCamera() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let controller = UIImagePickerController()
+            controller.delegate = self
+            controller.sourceType = UIImagePickerControllerSourceType.Camera
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+    // ライブラリから写真を選択する
+    func pickImageFromLibrary() {
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let controller = UIImagePickerController()
+            controller.delegate = self
+            controller.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
+    }
+    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
--- SegueTableViewDetailPic02/ViewController.swift
+++ SegueTableViewDetailPic02/ViewController.swift
@@ -10,18 +10,55 @@ import UIKit
 
 class ViewController: UIViewController , UITableViewDataSource, UITableViewDelegate{
     
-    @IBOutlet var tableView : UITableView?
+    
+    /*
+    let calendar = NSCalendar(identifier: NSGregorianCalendar)!
+    
+    class Event {
+        
+        let mainImage: UIImage? //メインの写真 //?はinitに必須, nilのある場合
+        let name: String //名前
+        // let data: NSMutableData //登録日時
+        // var data: NSData?
+        
+        // イニシャライザ
+        init(mainImage: UIImage, name: String/*, data: NSData*/) {
+            self.mainImage = nil
+            self.name = name
+//            self.data = ......
+//            self.data = nil
+        }
+        
+        // 説明
+//        func description() -> String {
+//            return "\(name) Lv.\(level)"
+    }
+//    
+//    let m = Monster()
+//    println(m.description()) // 不明 Lv.1
+*/
+    
+    @IBOutlet var tableView : UITableView!
     
     let imgArray: NSMutableArray = ["0.png","1.png","2.png","3.png","4.png","5.png","6.png","7.png"]
     
-    let label2Array: NSArray = ["2013/8/23 16:04","2013/8/23 16:15","2013/8/23 16:47","2013/8/23 17:10",
+    var label2Array = ["2013/8/23 16:04","2013/8/23 16:15","2013/8/23 16:47","2013/8/23 17:10",
         "2013/8/23 17:15","2013/8/23 17:21","2013/8/23 17:33","2013/8/23 17:41"]
     
     var selectedImage: UIImage?
     
+//    var eventArray = [Event]()
+
+//    eventArray.append("0.png","写真その０"/*,0*/)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //Addボタンの生成
+        let addButton = UIBarButtonItem(barButtonSystemItem: .Add, target: self, action: "insertNewObject:")
+        self.navigationItem.rightBarButtonItem = addButton
     }
+    
+   
     
     //Table Viewのセルの数を指定
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -57,15 +94,42 @@ class ViewController: UIViewController , UITableViewDataSource, UITableViewDeleg
         // DetailViewController へ遷移するために Segue を呼び出す
         performSegueWithIdentifier("ToDetailViewController",sender: nil)
     }
+    
+//    //
+//    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+//        if segue.identifier == "showDetail" {
+//            if let indexPath = self.tableView.indexPathForSelectedRow() {
+//                let object = objects[indexPath.row] as NSDate
+//                (segue.destinationViewController as DetailViewController).detailItem = object
+//            }
+//        }
+//    }
+    
+    
     // Add が選択された場合
+    func insertNewObject(sender: AnyObject) {
+        
+        let now = NSDate() //現在時刻を取得して文字列に変換
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm" //"yyyy/MM/dd HH:mm:ss"
+        let nowString = formatter.stringFromDate(now)
+        
+        label2Array.insert(nowString, atIndex: 0)
+        selectedImage = nil
+        let indexPath = NSIndexPath(forRow: 0, inSection: 0)
+        tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+        
+    }
     // DetailViewController に遷移
     
     // Segue 準備
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject!) {
         if (segue.identifier == "ToDetailViewController") {
-            let DetailVC: DetailViewController = segue.destinationViewController as DetailViewController
-            // DetailViewController のselectedImgに選択された画像を設定する
-            DetailVC.selectedImg = selectedImage
+            if selectedImage != nil{
+                let DetailVC: DetailViewController = segue.destinationViewController as DetailViewController
+                // DetailViewController のselectedImgに選択された画像を設定する
+                DetailVC.selectedImg = selectedImage
+            }
         }
     }
     
